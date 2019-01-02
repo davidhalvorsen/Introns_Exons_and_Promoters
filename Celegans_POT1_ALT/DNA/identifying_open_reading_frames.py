1c5c8bbf2febda4d40b3fabd85c37ada94f9cf79
davidhalvorsen <david.h.halvorsen@gmail.com>
2019-01-02 09:54:07 -0800

    post

---
+++ Celegans_POT1_ALT/DNA/identifying_open_reading_frames.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+# http://biopython.org/DIST/docs/tutorial/Tutorial.html
+# 20.1.13. Identifying open reading frames
+# https://biopython.readthedocs.io/en/latest/Tutorial/chapter_cookbook.html
+
+from Bio import SeqIO
+# record = SeqIO.read("NC_005816.fna", "fasta")
+file_0 = "NM_001361730.1_Caenorhabditis_elegans_pot-1_gene_homolog.fasta"
+file_1 = "NM_001361731.1_Caenorhabditis_elegans_pot-1_gene_homolog.fasta"
+file_2 = "NM_001361732.1_Caenorhabditis_elegans_pot-1_gene_homolog.fasta"
+file_3 = "NM_066157.3_pot-1_NCBI_DNA_matchesP42001_Celegans.fasta"
+
+print("")
+print("NM_001361730.1_Caenorhabditis_elegans_pot-1_gene_homolog.fasta")
+record = SeqIO.read(file_0, "fasta")
+table = 11
+min_pro_len = 100
+
+for strand, nuc in [(+1, record.seq), (-1, record.seq.reverse_complement())]:
+    for frame in range(3):
+        length = 3 * ((len(record)-frame) // 3) #Multiple of three
+        for pro in nuc[frame:frame+length].translate(table).split("*"):
+            if len(pro) >= min_pro_len:
+                print("%s...%s - length %i, strand %i, frame %i" \
+                % (pro[:30], pro[-3:], len(pro), strand, frame))
+print("")
+print("NM_001361731.1_Caenorhabditis_elegans_pot-1_gene_homolog.fasta")
+record = SeqIO.read(file_1, "fasta")
+table = 11
+min_pro_len = 100
+
+for strand, nuc in [(+1, record.seq), (-1, record.seq.reverse_complement())]:
+    for frame in range(3):
+        length = 3 * ((len(record)-frame) // 3) #Multiple of three
+        for pro in nuc[frame:frame+length].translate(table).split("*"):
+            if len(pro) >= min_pro_len:
+                print("%s...%s - length %i, strand %i, frame %i" \
+                % (pro[:30], pro[-3:], len(pro), strand, frame))
+print("")
+print("NM_001361732.1_Caenorhabditis_elegans_pot-1_gene_homolog.fasta")
+record = SeqIO.read(file_2, "fasta")
+table = 11
+min_pro_len = 100
+
+for strand, nuc in [(+1, record.seq), (-1, record.seq.reverse_complement())]:
+    for frame in range(3):
+        length = 3 * ((len(record)-frame) // 3) #Multiple of three
+        for pro in nuc[frame:frame+length].translate(table).split("*"):
+            if len(pro) >= min_pro_len:
+                print("%s...%s - length %i, strand %i, frame %i" \
+                % (pro[:30], pro[-3:], len(pro), strand, frame))
+
+print("")
+print("NM_066157.3_pot-1_NCBI_DNA_matchesP42001_Celegans.fasta")
+record = SeqIO.read(file_3, "fasta")
+table = 11
+min_pro_len = 100
+
+for strand, nuc in [(+1, record.seq), (-1, record.seq.reverse_complement())]:
+    for frame in range(3):
+        length = 3 * ((len(record)-frame) // 3) #Multiple of three
+        for pro in nuc[frame:frame+length].translate(table).split("*"):
+            if len(pro) >= min_pro_len:
+                print("%s...%s - length %i, strand %i, frame %i" \
+                % (pro[:30], pro[-3:], len(pro), strand, frame))
+
